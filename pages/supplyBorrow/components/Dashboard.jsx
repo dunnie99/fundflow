e6091961fc9a0a234675791f92e081210cb47eb1
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-23 15:12:20 +0100

    configure

---
+++ pages/supplyBorrow/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+
+
+const Dashboard = () => {
+  return (
+    <div className="w-[1281px] mx-auto h-[569px] bg-[#2F3677] mt-[20px] rounded-[36px]">
+                <div className="w-[1186px] mx-auto pt-[20px]">
+                <h1>Supply</h1>                        
+                </div>
+        </div>
+  )
+}
+
+export default Dashboard
+
+// className="w-[1186px] mx-auto border-b-[3px] border-b-[#8287AE] pt-[80px] h-[60px]"
--- pages/supplyBorrow/index.jsx
+++ pages/supplyBorrow/index.jsx
@@ -1,12 +1,11 @@
-
+import Title from "./components/Title"
+import Dashboard from "./components/Dashboard"
 
 const SupplyBorrow = () => {
   return (
     <div>
-        <div className="flex space-x-4">
-            <h1>Supply</h1>
-            <h2>Borrow</h2>
-        </div>
+        <Title />
+        <Dashboard />
     </div>
   )
 }
