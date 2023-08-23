b78d36404600a6e398d021e6b78cea2be0170c4e
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-23 12:24:21 +0100

    supply borrow, tailwind set up

--- pages/supplyBorrow/index.jsx
+++ pages/supplyBorrow/index.jsx
@@ -11,3 +11,16 @@ const SupplyBorrow = () => {
 }
 
 export default SupplyBorrow
+
+const SupplyBorrow = () => {
+  return (
+    <div>
+        <div className="flex space-x-4">
+            <h1>Supply</h1>
+            <h2>Borrow</h2>
+        </div>
+    </div>
+  )
+}
+
+export default SupplyBorrow
