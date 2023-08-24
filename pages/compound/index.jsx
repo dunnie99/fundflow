ca9891c868519fbbd9770d063ba9ff36d950c7d3
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-24 09:09:00 +0100

    update

--- pages/compound/index.jsx
+++ pages/compound/index.jsx
@@ -19,6 +19,7 @@ const Compound = () => {
     <Layout>
         <AppHeadNav/>
     <div>
+    <AppHeadNav />
     <div className="flex gap-8 mono_font justify-center mx-auto pt-[100px] text-[#2F3677] relative">
             <h1 onClick={()=>setOpen(true)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${open && "border-b-[2px]"} border-b-[#2F3677]`}>Supply</h1>
             <h2 onClick={()=>setOpen(false)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${!open && "border-b-[2px]"} border-b-[#2F3677]`}>Borrow</h2>
