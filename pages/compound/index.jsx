a2768ec95f76a0b7d079510b576a8030eb604399
vicsnet <adesanmiadebisi@gmail.com>
2023-08-24 09:40:24 +0100

    swap and borrow ui adjusted

--- pages/compound/index.jsx
+++ pages/compound/index.jsx
@@ -3,11 +3,10 @@ import Supply from "./components/Supply"
 import Layout from "../../components/Layout"
 import { useState } from "react"
 import Borrow from "./components/Borrow"
-"use client"
-import Supply from "./components/Supply"
-import Layout from "../../components/Layout"
-import { useState } from "react"
-import Borrow from "./components/Borrow"
+import { AppHeadNav } from "../../components/AppHeadNav"
+import { GlobalContext } from "../../context/GlobalContext"
+
+
 import { AppHeadNav } from "../../components/AppHeadNav"
 
 const Compound = () => {
@@ -16,20 +15,8 @@ const Compound = () => {
   const [open, setOpen] = useState(false)
   return (
     <Layout>
-    <Layout>
         <AppHeadNav/>
     <div>
-    <AppHeadNav />
-    <div className="flex gap-8 mono_font justify-center mx-auto pt-[100px] text-[#2F3677] relative">
-            <h1 onClick={()=>setOpen(true)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${open && "border-b-[2px]"} border-b-[#2F3677]`}>Supply</h1>
-            <h2 onClick={()=>setOpen(false)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${!open && "border-b-[2px]"} border-b-[#2F3677]`}>Borrow</h2>
-        </div>
-    {
-      open ?
-      <Supply />
-      :
-        <Borrow/>
-    }
     <div className="flex gap-8 mono_font justify-center mx-auto pt-[30px] text-[#2F3677] relative">
             <h1 onClick={()=>setOpen(true)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${open && "border-b-[2px]"} border-b-[#2F3677]`}>Supply</h1>
             <h2 onClick={()=>setOpen(false)} className={`cursor-pointer py-[12px] font-[400] text-center text-[32px] text-[#2F3677] ${!open && "border-b-[2px]"} border-b-[#2F3677]`}>Borrow</h2>
@@ -42,9 +29,7 @@ const Compound = () => {
     }
     </div>
     </Layout>
-    </Layout>
   )
 }
 
-export default Compound
 export default Compound
